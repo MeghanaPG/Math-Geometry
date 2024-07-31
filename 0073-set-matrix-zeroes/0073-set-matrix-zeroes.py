f56f9cb7c59264d32193065c6ab4c2a56320efb8
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-07-31 10:11:48 -0400

    Time: 105 ms (42.82%), Space: 17.5 MB (15.74%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,41 +3,33 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Time Complexity: O(1), O(m.n)
-        # Space/memory complexity: O(1)
-
         ROWS, COLS = len(matrix), len(matrix[0])
-        # we need one extra variable to tell us if the first row is zero or not 
+
         rowZero = False
 
-        # determine which rows/cols needs to be zero
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
-                    # we are gonna set that row to be zero 
-                    matrix[0][c] = 0 
+                    # first set that row to be zero
+                    matrix[0][c] = 0
 
                     if r > 0:
-                        matrix[r][0] = 0 
+                        matrix[r][0] = 0
                     else:
                         rowZero = True 
-        
-        # we zero out most of them          
+                    
+            
         for r in range(1, ROWS):
             for c in range(1, COLS):
-                # for each position we are gonna check if we need to zero it out 
                 if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0 
-
-        # we zero out first column 
+        
+        # handling first column
         if matrix[0][0] == 0:
             for r in range(ROWS):
-                # zeroing out the first column 
                 matrix[r][0] = 0 
 
-        # Lastly we need to zero out first row if need to 
+        # handling the first row 
         if rowZero:
             for c in range(COLS):
-                matrix[0][c] = 0 
-
-
+                matrix[0][c] = 0
