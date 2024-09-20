788d36ebe4855100c1490e9c61d95c3d5d9c2042
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-20 07:46:48 -0400

    Time: 28 ms (97.70%), Space: 16.6 MB (8.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -7,24 +7,30 @@ class Solution:
         l, r = 0, len(matrix) - 1
 
         while l < r:
-            for i in range(r-l):
-                top, bottom = l, r
-
-                # save the top left val 
+            for i in range(r - l):
+                top, bottom = l , r 
                 topLeft = matrix[top][l+i]
 
-                # move bottom left into top left 
                 matrix[top][l+i] = matrix[bottom - i][l]
 
-                # move bottom right to bottom left 
-                matrix[bottom-i][l] = matrix[bottom][r-i]
+                matrix[bottom - i][l] = matrix[bottom][r-i]
 
-                # move top right to bottom right 
-                matrix[bottom][r-i] = matrix[top + i][r]
+                matrix[bottom][r-i] = matrix[top+i][r]
 
-                #move top left into top right 
-                matrix[top + i][r] = topLeft 
-            r -= 1 
-            l += 1 
-            
+                matrix[top+i][r] = topLeft 
+            r -= 1
+            l += 1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
 
